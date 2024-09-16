39edc17d4f93426b47faa52921a25532616ad572
fast-facts <107660271+fast-facts@users.noreply.github.com>
2024-09-16 18:29:56 -0400

    chore: Updated eslint config formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,10 +14,10 @@ export default tseslint.config(
 
     languageOptions: {
       ecmaVersion: 2020,
-      sourceType: "module",
+      sourceType: 'module',
 
       parserOptions: {
-        project: resolve(import.meta.dirname, "tsconfig.json")
+        project: resolve(import.meta.dirname, 'tsconfig.json')
       }
     },
 
@@ -26,49 +26,49 @@ export default tseslint.config(
     },
 
     rules: {
-      "constructor-super": ["error"],
-      curly: ["error", "all"],
-      "default-case": ["error"],
+      'constructor-super': ['error'],
+      curly: ['error', 'all'],
+      'default-case': ['error'],
 
-      eqeqeq: ["error", "always", {
-        null: "ignore",
+      eqeqeq: ['error', 'always', {
+        null: 'ignore',
       }],
 
-      "guard-for-in": ["error"],
-      "new-parens": ["error"],
-      "no-bitwise": ["error"],
-      "no-caller": ["error"],
-      "no-cond-assign": ["error"],
+      'guard-for-in': ['error'],
+      'new-parens': ['error'],
+      'no-bitwise': ['error'],
+      'no-caller': ['error'],
+      'no-cond-assign': ['error'],
 
-      "no-console": ["warn", {
-        allow: ["warn", "error"],
+      'no-console': ['warn', {
+        allow: ['warn', 'error'],
       }],
 
-      "no-debugger": ["error"],
-      "no-eval": ["error"],
-      "no-fallthrough": ["error"],
-      "no-new-wrappers": ["error"],
-      "no-sparse-arrays": ["error"],
-      "no-template-curly-in-string": ["error"],
-      "no-unsafe-finally": ["error"],
-      "no-unused-expressions": ["warn"],
-      "no-unused-labels": ["error"],
-      "no-var": ["error"],
-      "one-var": ["error", "never"],
-      "prefer-template": ["error"],
-      radix: ["error"],
+      'no-debugger': ['error'],
+      'no-eval': ['error'],
+      'no-fallthrough': ['error'],
+      'no-new-wrappers': ['error'],
+      'no-sparse-arrays': ['error'],
+      'no-template-curly-in-string': ['error'],
+      'no-unsafe-finally': ['error'],
+      'no-unused-expressions': ['warn'],
+      'no-unused-labels': ['error'],
+      'no-var': ['error'],
+      'one-var': ['error', 'never'],
+      'prefer-template': ['error'],
+      radix: ['error'],
 
-      "sort-keys": "off",
+      'sort-keys': 'off',
 
-      "use-isnan": ["error"],
-      "@typescript-eslint/adjacent-overload-signatures": ["error"],
+      'use-isnan': ['error'],
+      '@typescript-eslint/adjacent-overload-signatures': ['error'],
 
-      "@typescript-eslint/array-type": ["error", {
-        default: "array",
-        readonly: "array",
+      '@typescript-eslint/array-type': ['error', {
+        default: 'array',
+        readonly: 'array',
       }],
 
-      // "@typescript-eslint/ban-types": ["error", {
+      // '@typescript-eslint/ban-types': ['error', {
       //   extendDefaults: true,
 
       //   types: {
@@ -76,75 +76,75 @@ export default tseslint.config(
       //   },
       // }],
 
-      // "@typescript-eslint/member-ordering": ["warn", {
+      // '@typescript-eslint/member-ordering': ['warn', {
       //   classes: {
       //     memberTypes: [
-      //       "public-static-field",
-      //       "protected-static-field",
-      //       "private-static-field",
-      //       "public-static-method",
-      //       "protected-static-method",
-      //       "private-static-method",
-      //       "public-instance-field",
-      //       "protected-instance-field",
-      //       "private-instance-field",
-      //       "public-abstract-field",
-      //       "protected-abstract-field",
-      //       "private-abstract-field",
-      //       "public-constructor",
-      //       "protected-constructor",
-      //       "private-constructor",
-      //       "public-instance-method",
-      //       "protected-instance-method",
-      //       "private-instance-method",
-      //       "public-abstract-method",
-      //       "protected-abstract-method",
-      //       "private-abstract-method",
+      //       'public-static-field',
+      //       'protected-static-field',
+      //       'private-static-field',
+      //       'public-static-method',
+      //       'protected-static-method',
+      //       'private-static-method',
+      //       'public-instance-field',
+      //       'protected-instance-field',
+      //       'private-instance-field',
+      //       'public-abstract-field',
+      //       'protected-abstract-field',
+      //       'private-abstract-field',
+      //       'public-constructor',
+      //       'protected-constructor',
+      //       'private-constructor',
+      //       'public-instance-method',
+      //       'protected-instance-method',
+      //       'private-instance-method',
+      //       'public-abstract-method',
+      //       'protected-abstract-method',
+      //       'private-abstract-method',
       //     ],
 
-      //     order: "alphabetically",
+      //     order: 'alphabetically',
       //   },
       // }],
 
-      "@typescript-eslint/naming-convention": ["warn", {
-        format: ["camelCase", "PascalCase", "UPPER_CASE"],
-        leadingUnderscore: "allow",
-        selector: "variableLike",
-        trailingUnderscore: "allow",
+      '@typescript-eslint/naming-convention': ['warn', {
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+        selector: 'variableLike',
+        trailingUnderscore: 'allow',
       }, {
-          format: ["PascalCase"],
-          leadingUnderscore: "allow",
-          selector: "typeLike",
-          trailingUnderscore: "allow",
+          format: ['PascalCase'],
+          leadingUnderscore: 'allow',
+          selector: 'typeLike',
+          trailingUnderscore: 'allow',
         }],
 
-      "@typescript-eslint/no-extra-non-null-assertion": ["error"],
-      "@typescript-eslint/no-inferrable-types": ["warn"],
-      "@typescript-eslint/no-invalid-this": ["error"],
-      "@typescript-eslint/no-namespace": ["error"],
-      "@typescript-eslint/no-non-null-asserted-optional-chain": ["error"],
-      "@typescript-eslint/no-non-null-assertion": ["error"],
-      "@typescript-eslint/no-unnecessary-boolean-literal-compare": ["warn"],
+      '@typescript-eslint/no-extra-non-null-assertion': ['error'],
+      '@typescript-eslint/no-inferrable-types': ['warn'],
+      '@typescript-eslint/no-invalid-this': ['error'],
+      '@typescript-eslint/no-namespace': ['error'],
+      '@typescript-eslint/no-non-null-asserted-optional-chain': ['error'],
+      '@typescript-eslint/no-non-null-assertion': ['error'],
+      '@typescript-eslint/no-unnecessary-boolean-literal-compare': ['warn'],
 
-      "@typescript-eslint/no-unused-vars": ["warn", {
-        args: "none",
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        args: 'none',
         ignoreRestSiblings: true,
       }],
 
-      "@typescript-eslint/prefer-for-of": ["warn"],
-      "@typescript-eslint/prefer-nullish-coalescing": ["error"],
+      '@typescript-eslint/prefer-for-of': ['warn'],
+      '@typescript-eslint/prefer-nullish-coalescing': ['error'],
 
-      "@typescript-eslint/require-array-sort-compare": ["error", {
+      '@typescript-eslint/require-array-sort-compare': ['error', {
         ignoreStringArrays: true,
       }],
 
-      "@typescript-eslint/require-await": ["error"],
-      "@typescript-eslint/return-await": ["error"],
+      '@typescript-eslint/require-await': ['error'],
+      '@typescript-eslint/return-await': ['error'],
 
-      "@typescript-eslint/triple-slash-reference": ["error", {
-        lib: "never",
-        path: "never",
-        types: "never",
+      '@typescript-eslint/triple-slash-reference': ['error', {
+        lib: 'never',
+        path: 'never',
+        types: 'never',
       }],
     }
   }
